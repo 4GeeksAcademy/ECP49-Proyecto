fd8d5433eeed6c2022745bd3bd3dbf59eb62ad27
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2024-01-14 11:43:44 +0000

    get all Videogames list front end

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,52 +1,53 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+				videogames: []			
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+      // Use getActions to call a function within a function
+      getVideogames: () => {
+		fetch(process.env.BACKEND_URL + "/api/videogames")
+		  .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            setStore({ videogames: data });
+          })
+          .catch((err) => console.error(err));
+	},
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			
+			
+			
+			// exampleFunction: () => {
+			// 	getActions().changeColor(0, "green");
+			// },
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+			// getMessage: async () => {
+			// 	try{
+			// 		// fetching data from the backend
+			// 		const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
+			// 		const data = await resp.json()
+			// 		setStore({ message: data.message })
+			// 		// don't forget to return something, that is how the async resolves
+			// 		return data;
+			// 	}catch(error){
+			// 		console.log("Error loading message from backend", error)
+			// 	}
+			// },
+			// changeColor: (index, color) => {
+			// 	//get the store
+			// 	const store = getStore();
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			// 	//we have to loop the entire demo array to look for the respective index
+			// 	//and change its color
+			// 	const demo = store.demo.map((elm, i) => {
+			// 		if (i === index) elm.background = color;
+			// 		return elm;
+			// 	});
+
+			// 	//reset the global store
+			// 	setStore({ demo: demo });
+			// }
 		}
 	};
 };
