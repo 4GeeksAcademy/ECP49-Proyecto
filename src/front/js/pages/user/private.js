4b639519bf0a2892b8f469aadf7247d487308daf
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2024-01-24 19:32:44 +0000

    navbar sign

--- src/front/js/pages/user/private.js
+++ src/front/js/pages/user/private.js
@@ -1,11 +1,11 @@
-import React, {useEffect, useContext} from "react";
+import React, { useEffect, useContext } from "react";
 import { Context } from "../../store/appContext";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 
 const Private = () => {
-    const {store, actions} = useContext(Context);
+    const { store, actions } = useContext(Context);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -13,20 +13,21 @@ const Private = () => {
             actions.authenticateUser(navigate);
         }
         setTimeout(() => {
-            authenticate() }, 500)        
+            authenticate()
+        }, 500)
     }, [])
 
     return (
         <div className="container text-center">
             <h1>Hello!</h1>
-            {store.user!= null ?
+            {store.user != null ?
                 <div >
                     <h2>Email: {store.user.email}</h2>
                 </div>
                 :
                 navigate("/login")
             }
-            <Link to="/">Go to Home</Link>
+            <Link to="/" className="btn btn-outline-danger">Go to Home</Link>
         </div>
     );
 }
