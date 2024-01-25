d46b66d757446d75e690ccff28ddeb5906c7197c
Edo <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2024-01-25 18:07:25 +0000

    Botón de delete videogame funcionando correctamente

--- src/front/js/pages/videogames/videogameList.js
+++ src/front/js/pages/videogames/videogameList.js
@@ -1,86 +1,80 @@
-import React, { useContext, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
-import '../../../styles/videogames.css'
+import { Link } from "react-router-dom";
+
 import { Context } from "../../store/appContext";
-import { FormVideogame } from "./formVideogame";
 
-export const VideogamesList = ({ videogame }) => {
-  const { store, actions } = useContext(Context);
+export const VideogamesList = () => {
+    const { store, actions } = useContext(Context);
 
-  const [showForm, setShowForm] = useState(false);
+    const handleDeleteVideogame = (videogame_id) => {
+        actions.deleteVideogame(videogame_id);
+    };
 
-  const handleToggleForm = () => {
-    setShowForm(!showForm);
-  };
+    return (
+        <div className="container">
+            <ul className="list-group">
+                {store.videogames == false && <p>...loading</p>}
+                {store.videogames.length > 0 &&
+                    store.videogames.map((videogame, index) => {
+                        return (
+                            <li
+                                key={index}
+                                className="list-group-item d-flex justify-content-between"
+                            >
+                                <Link to={"/videogames/" + index} className="text-decoration-none">
+                                    <span>{videogame.name}</span>
+                                    <span>{videogame.pegi}</span>
+                                    <span>{videogame.year}</span>
+                                </Link>
 
-  const handleUpdate = (videogame) => {
-    // Update the videogame in the store
-    actions.updateVideogame(videogame);
+                                <Link
+                                    className="btn btn-success"
+                                    to={"/videogames/" + index}
+                                >
+                                    Learn More...
+                                </Link>
 
-    // Hide the form
-    setShowForm(false);
-  };
-  return (
-    <div className="container">
-      <button onClick={handleToggleForm}>Add New Videogame</button>
-      {showForm ? (
-        <FormVideogame
-          videogame={videogame}
-          onUpdate={handleUpdate}
-          onCancel={() => setShowForm(false)}
-        />
-      ) : null}
-      <br />
-      <div className="">
-        <div className="d-flex">
 
-          <ul className="list-group list-group-horizontal row" >
-            {store.videogames == false && (
-              <p>...loading</p>
-            )}
-            {store.videogames.length > 0 &&
-              store.videogames.map((videogame, index) => {
-                return (
-                  <>
-                    <span
-                      key={index}
-                      className="col-3"
-                    >
-                      <Link to={"/videogames/" + index} className="text-decoration-none" >
-                        <>
-                          <div className="card">
-                            <div className="card-body list-group ">
-                              <span className="card-title">Name: {videogame.name}</span>
-                              <span className="card-title">Pegi: {videogame.pegi}</span>
-                              <span className="card-title">Year: {videogame.year}</span>
-                              <Link
-                                className="btn btn-success"
-                                to={'/videogames/' + index}
-                              >
-                                Learn More...
-                              </Link>
-                            </div>
-                          </div>
-                        </>
-                      </Link>
+                                
+                                    <Link to={`/consoles/edit/${console.id}`}>
+                                        <button className="btn btn-primary">Edit</button>
+                                    </Link>
+                                    
+
+                               
+                                    <button
+                                        className="btn btn-danger"
+                                        onClick={() => handleDeleteVideogame(videogame.id)}>
+                                        Delete
+                                    </button>
+                                    
+
+
+                            </li>
+                        );
+                    })}
+            </ul>
+            <br />
+
+            <Link to="/">
+                <button className="btn btn-primary">Back home</button>
+            </Link>
+
+           
+                <Link to="/consoles/add">
+                    <span className="btn btn-primary" href="#" role="button">
+                        Add videogame
                     </span>
-                  </>
-                );
-              })}
-          </ul>
-        </div>
-      </div>
-      <br />
+                </Link>
+               
 
-      <Link to="/">
-        <button className="btn btn-primary">Back home</button>
-      </Link>
-    </div>
-  );
+        </div>
+    );
 };
+
 VideogamesList.propTypes = {
-  name: PropTypes.string,
-  pegi: PropTypes.number,
-  year: PropTypes.number,
+    name: PropTypes.string,
+    pegi: PropTypes.number,
+    year: PropTypes.number,
 };
