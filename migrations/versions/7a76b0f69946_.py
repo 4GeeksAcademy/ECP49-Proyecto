2adcd6c3560ff0f162a65ad6f38d2140acb4d27a
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2024-01-27 18:54:27 +0000

    busccador en add videogames

--- migrations/versions/7a76b0f69946_.py
+++ migrations/versions/7a76b0f69946_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 2ff4ea29a19f
+Revision ID: 7a76b0f69946
 Revises: 
-Create Date: 2024-01-25 18:56:57.212659
+Create Date: 2024-01-26 19:33:47.354662
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '2ff4ea29a19f'
+revision = '7a76b0f69946'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/front/js/pages/videogames/game.js
+++ src/front/js/pages/videogames/game.js
@@ -2,42 +2,58 @@ import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 
 export const Game = () => {
-  const [selectedGame, setSelectedGame] = useState(null);
-  const { gameId } = useParams();
+    const [selectedGame, setSelectedGame] = useState(null);
+    const { gameId } = useParams();
 
-  useEffect(() => {
-    const fetchRawgData = async () => {
-      try {
-        const response = await fetch(`https://api.rawg.io/api/games/${gameId}?key=e8722d91c21d4eec9047a9a02fd9efe7`);
-        const data = await response.json();
+    useEffect(() => {
+        const fetchRawgData = async () => {
+            try {
+                const response = await fetch(`https://api.rawg.io/api/games/${gameId}?key=e8722d91c21d4eec9047a9a02fd9efe7`);
+                const data = await response.json();
 
-        // Verificar si hay resultados y seleccionar el juego
-        if (data) {
-          setSelectedGame(data);
-        }
-      } catch (error) {
-        console.error("Error fetching data from RAWG API:", error);
-      }
-    };
+                // Verificar si hay resultados y seleccionar el juego
+                if (data) {
+                    setSelectedGame(data);
+                }
+            } catch (error) {
+                console.error("Error fetching data from RAWG API:", error);
+            }
+        };
 
-    fetchRawgData();
-  }, [gameId]);
+        fetchRawgData();
+    }, [gameId]);
 
-  return (
-    <>
-      <div className="">
-        {selectedGame ? (
-          <>
-            <h2>{selectedGame.name}</h2>
-            <p>Description: {selectedGame.description || "No description available"}</p>
-            {/* <p>Released: {selectedGame.released || "Unknown"}</p> */}
-            {/* <p>Rating: {selectedGame.rating || "Not rated"}</p> */}
-            {/* Puedes agregar más información según la estructura de datos de la API */}
-          </>
-        ) : (
-          <h2>No game selected</h2>
-        )}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="card" style={{maxWidth: "18rem"}}>
+                {selectedGame ? (
+
+                    <>
+                        <img className="img-thumbnail" style={{ maxheight: "150px", bottom: "0px" }} src={selectedGame.background_image} alt="Card image cap" />
+
+                        <div className="card text-center">
+
+                            <div className="card-header">
+                                <h2>{selectedGame.name}</h2>
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">
+                                </h5>
+                                <p className="card-text">Year: {selectedGame.released || "Unknown"}</p>
+                                <p className="card-text">Pegi: {selectedGame.pegi || "Not defined"}</p>
+                            </div>
+                            <div className="card-footer text-muted">
+                                <h2>{selectedGame.name}</h2>
+                            </div>
+
+                        </div>
+                    </>
+
+                ) : (
+                    <h2>No game selected</h2>
+                )}
+
+            </div>
+        </>
+    );
 };
