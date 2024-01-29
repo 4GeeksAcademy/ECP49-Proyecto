cc907769ca81e5b31467f571cdc6e6cce72b4321
Edo <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2024-01-29 20:31:39 +0000

    Trabajo de estilo visual

--- src/front/js/pages/consoles/consoleListContainer.js
+++ src/front/js/pages/consoles/consoleListContainer.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link, useParams } from 'react-router-dom';
 import { Context } from '../../store/appContext';
+import '../../../styles/listContainers.css';
 
 
 export const ConsoleListContainer = () => {
@@ -9,7 +10,7 @@ export const ConsoleListContainer = () => {
     const params = useParams();
   
     return (
-      <div className="jumbotron container">
+      <div className="jumbotron jumbotronList container">
         <div className="d-flex justify-content-center overflow row">
           <div className="col">
             <Link to={"/Consoles/"} className="text-decoration-none">
@@ -26,15 +27,15 @@ export const ConsoleListContainer = () => {
                   <span key={index} className="">
                     <Link to={"/consoles/" + index} className="text-decoration-none">
                       <>
-                        <div className="card-body list-group">
-                          <span className="card-title">Name: {console.name}</span>
-                          <span className="card-title">Company: {console.company}</span>
-                          <span className="card-title">Year: {console.year}</span>
+                        <div className="card-body card-bodyList  list-group">
+                          <span className="card-titleList1">Name: {console.name}</span>
+                          <span className="card-titleList2">Company: {console.company}</span>
+                          <span className="card-titleList3">Year: {console.year}</span>
                           <Link
-                            className="btn btn-success"
+                            className="btn btn-success moreinfo"
                             to={'/consoles/' + index}
                           >
-                            Learn More...
+                            More info
                           </Link>
                         </div>
                       </>
