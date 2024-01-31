5559bb176b3fce42bac039d7a7857f39e24d94e4
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2024-01-31 11:42:08 +0000

    estilos navbar y footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,25 +1,29 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
+import '../../styles/home.css'
+
 
 export const Footer = () => {
   const { store } = useContext(Context);
 
   return (
-    <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 mx-4 w-75 border-top">
+    <div className="container-fluid fondo_nav">
+    <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 px-5  border-top fixed-bottom fondo_nav">
         <Link to="/" className="btn btn-outline-danger">
           Go to Home
         </Link>
 
         {store.user && (
-          <p>
+          <p style={{backgroundColor: "#C3E2C2"}}>
             Logged in as: {store.user.email}
           </p>
         )}
-        <p>
+        <p style={{backgroundColor: "#C3E2C2"}}>
           Made with <i className="fa fa-heart text-danger" /> by{" "}
-          <a href="http://www.4geeksacademy.com">4Geeks Academy, Group 49</a>
+          <a href="http://www.4geeksacademy.com" style={{backgroundColor: "#C3E2C2"}}>4Geeks Academy, Group 49</a>
         </p>
     </footer>
+    </div>
   );
 };
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,42 +3,46 @@ import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
 import { SearchBar } from "./searchBar.js"
+import '../../styles/home.css'
 
 export const Navbar = () => {
 	const navigate = useNavigate();
 	const { store, actions } = useContext(Context);
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
+		<nav className="navbar">
+			<div className="container fondo_nav page-footer">
 				<Link to="/">
-					<button className="btn btn-danger mb-0 h1"><i class="fa-solid fa-house-user"></i></button>
-				</Link>
-
-				<Link to="/userProfile">
-					<button className="btn btn-danger mb-0 h1">My Profile</button>
+					<button className="btn btn-danger mb-0 h1"><i className="fa-solid fa-house-user btn-danger"></i></button>
 				</Link>
 
 				<Link to="/adminform">
-					{store.auth === true ? <button onClick={() => actions.logoutadmin()} className="btn btn-danger" ><i class="fa-solid fa-right-from-bracket mx-2"></i>logoutADMIN</button> : <button className="btn btn-primary">ADMIN</button>}
+					{store.auth === true ? <button onClick={() => actions.logoutadmin()} className="btn btn-danger" ><i className="fa-solid fa-right-from-bracket mx-2 btn-primary"></i>logout Admin</button> : <button className="btn btn-primary"><i className="fa fa-key mx-2 btn-primary" aria-hidden="true"></i>
+						Admin</button>}
 				</Link>
 
-			{/* ++ SEARCH BUTTON ++ */}
+				{/* ++ SEARCH BUTTON ++ */}
 				<SearchBar />
-				<div className="ml-auto">
+				<div className="ml-auto navbar">
+					<Link to="/userProfile">
+						<button className="btn btn-danger mb-0 h1"><i className="fa fa-user btn-danger" aria-hidden="true"></i>
+						</button>
+					</Link>
 					{store.user ?
 						<>
 							<Link to="/signup" className="mx-2">
-								<button className="btn btn-primary"><i className="fa-solid fa-user-plus mx-2"></i>Signup</button>
+								<button className="btn btn-primary"><i className="fa-solid fa-user-plus mx-2 btn-primary"></i>Signup</button>
 							</Link>
+							<div className="vr"></div>
 							<Link to="/login">
-								<button className="btn btn-primary"><i className="fas fa-sign-in mx-2"></i>Login</button>
+								<button className="btn btn-primary"><i className="fas fa-sign-in mx-2 btn-primary"></i>Login</button>
 							</Link>
 						</>
 						:
 						<>
 							<Link to="/private">
-								<button className="btn btn-primary">User</button>
+								<button className="btn btn-primary btn-primary">User</button>
 							</Link>
+							<div className="vr"></div>
 							<button className="btn btn-primary" onClick={() => actions.logout()}>Log Out</button>
 						</>
 					}
